05de1e79a7d586ab093008e72d1f8a8659e2dfc6
Ben Mercer <104690692+benjamano@users.noreply.github.com>
2022-09-28 20:37:07 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,18 @@
-let x = 50
-let y = 10
+let x = 10
+let y = 65
 kitronik_servo_lite.setDistancePerSecond(10)
 basic.showIcon(IconNames.Happy)
 let strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
 strip.showRainbow(1, 360)
 basic.forever(function () {
-    while (true) {
-        strip.shift(1)
-    }
     kitronik_servo_lite.driveForwards(x)
     kitronik_servo_lite.turnLeft(y)
-    y += 10
+    kitronik_servo_lite.driveForwards(x)
+    kitronik_servo_lite.turnLeft(y)
+    kitronik_servo_lite.driveForwards(x)
+    kitronik_servo_lite.turnLeft(y)
+    kitronik_servo_lite.driveForwards(x)
+    kitronik_servo_lite.turnLeft(y)
+    y += 5
+    y += 2
 })
